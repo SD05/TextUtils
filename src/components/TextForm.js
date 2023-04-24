3aedcfe34270354a8a46bcb353bcf832c0719429
Shubham Kumar Sahu <sdimension0506@gmail.com>
2023-04-25 00:10:13 +0530

    Added Dark Mode functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -33,7 +33,12 @@ function TextForm(props) {
 
   return (
     <>
-      <div>
+      <div
+        className="container"
+        style={{
+          color: props.mode === "dark" ? "white" : "#042743",
+        }}
+      >
         <h1>{props.heading}</h1>
         <div className="mb-3">
           <textarea
@@ -42,6 +47,10 @@ function TextForm(props) {
             id="myBox"
             onChange={onChange}
             rows="8"
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "#042743",
+            }}
           ></textarea>
         </div>
         <button className="btn btn-primary mx-1" onClick={handleUpClick}>
@@ -60,13 +69,22 @@ function TextForm(props) {
           Remove Extra Spaces
         </button>
       </div>
-      <div className="container my-3">
+      <div
+        className="container my-3"
+        style={{
+          color: props.mode === "dark" ? "white" : "#042743",
+        }}
+      >
         <h1>Your text Summary</h1>
         <p>
           {text.split(" ").length} words and {text.length} characters
         </p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
-        <h3>{text}</h3>
+        <h3>
+          {text.length > 0
+            ? text
+            : "Enter something in the textbox above to preview it here"}
+        </h3>
       </div>
     </>
   );
